e4309c3f5e897f34fd8c959f5fb91f7e1183d83d
taaniagonzaalez <tania.gonzalez@estudiantat.upc.edu>
2026-03-18 18:23:52 +0100

    Add clients
    Divide the file into different scripts (pt3)

---
+++ clients.py
@@ -0,0 +1,48 @@
+from typing import List, Optional
+from config import SEMANTIC_SCHOLAR_API, DBLP_PUBL_API
+from utils import request_json, s2_headers
+
+class SemanticScholarClient:
+    def search_papers(self, query: str, limit: int = 20) -> List[dict]:
+        url = f"{SEMANTIC_SCHOLAR_API}/paper/search"
+        fields = ",".join([
+            "paperId", "title", "year", "venue", "authors",
+            "fieldsOfStudy", "citationCount", "referenceCount"
+        ])
+        params = {
+            "query": query,
+            "limit": limit,
+            "fields": fields
+        }
+        data = request_json(url, params=params, headers=s2_headers())
+        return data.get("data", [])
+
+    def get_paper_details(self, paper_id: str) -> dict:
+        url = f"{SEMANTIC_SCHOLAR_API}/paper/{paper_id}"
+        fields = ",".join([
+            "paperId", "externalIds", "title", "abstract", "year",
+            "venue", "authors", "fieldsOfStudy", "publicationTypes",
+            "journal", "referenceCount", "citationCount", "publicationDate",
+            "references.paperId", "references.title",
+            "citations.paperId", "citations.title"
+        ])
+        params = {"fields": fields}
+        return request_json(url, params=params, headers=s2_headers())
+
+
+class DBLPClient:
+    def search_publication_by_title(self, title: str) -> Optional[dict]:
+        params = {
+            "q": title,
+            "h": 1,
+            "format": "json"
+        }
+        try:
+            data = request_json(DBLP_PUBL_API, params=params, sleep=0.2)
+            hits = data.get("result", {}).get("hits", {}).get("hit", [])
+            if not hits:
+                return None
+            info = hits[0].get("info", {})
+            return info
+        except Exception:
+            return None
